0e728031718d3ed441a681c89b0ab378a02647aa
Dmitry Umnov <umnov.dm@gmail.com>
2023-08-01 13:13:02 +0400

    fix calendar dates bounds

--- app/src/main/java/com/umnvd/booking/domain/events/consts/DateConstraints.kt
+++ app/src/main/java/com/umnvd/booking/domain/events/consts/DateConstraints.kt
@@ -1,6 +1,8 @@
 package com.umnvd.booking.domain.events.consts
 
 import java.time.LocalDate
+import java.time.temporal.ChronoUnit
 
 val MIN_DATE: LocalDate = LocalDate.of(2023, 1, 1)
-val MAX_DATE: LocalDate = MIN_DATE.plusDays(365)
+val MAX_DATE: LocalDate = LocalDate.of(2023, 12, 31)
+val DAYS_COUNT = ChronoUnit.DAYS.between(MIN_DATE, MAX_DATE).toInt() + 1
